416f2a8f48013dc72382829f70a4f2892149625b
IsaacFr99 <132420909+IsaacFr99@users.noreply.github.com>
2023-05-02 22:30:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,11 @@ include("marco-right.php");
    <div  style="width: 100%;">     
     <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
        <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
-        	<h1>¿Quién es Alan Ramirez?</h1>
+        	<h1>¿Quién es Isaac Franco ?</h1>
        		 <hr>
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
-                Posteado por:  Alan Ramirez<br>
+                Posteado por:  Isaac Franco<br>
                 Fecha: 2023-04-02 <br>
           </div>
         </div>
@@ -24,9 +24,9 @@ include("marco-right.php");
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">¡Hola! Me complace presentarme, soy Ramírez Mera <b> Alan Ricardo </b>, estudiante de Ingeniería en Tecnologías de la Información en la Universidad Técnica Luis Vargas Torres de Esmeraldas. </p>
-                   <p class="eys-parrafo">Me apasiona el mundo de la tecnología y todo lo que tenga que ver con la informática, la programación y el desarrollo de software.</p>
-                   <p class="eys-parrafo">Mi objetivo a largo plazo es convertirme en un profesional exitoso y respetado en el campo de la tecnología, y contribuir al desarrollo de soluciones innovadoras que puedan mejorar la vida de las personas y la sociedad en general. Para lograrlo, estoy constantemente en busca de nuevas oportunidades de aprendizaje y crecimiento profesional, además de mantenerme actualizado sobre las últimas tendencias y avances en la industria.</p>
+                   <p class="eys-parrafo">¡Hola! Me complace presentarme, soy Franco Valdez <b>Walter Isaac </b>, estudiante de Ingeniería en Tecnologías de la Información en la Universidad Técnica Luis Vargas Torres de Esmeraldas. </p>
+                   <p class="eys-parrafo">Desde pequeño, siempre me ha apasionado la tecnología y todo lo relacionado con el mundo digital. Es por eso que decidí estudiar esta carrera para poder seguir aprendiendo sobre el desarrollo de software, el análisis de datos y la ciberseguridad.</p>
+                   <p class="eys-parrafo">Además de mis estudios, me gusta mantenerme actualizado sobre las últimas tendencias y avances en el mundo de la tecnología, por lo que dedico parte de mi tiempo libre a investigar y leer artículos sobre el tema.</p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
@@ -34,24 +34,25 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en la  Unidad Educativa Fiscal "Fausto Molina", donde me gradue como Bachiller en Ciencias. Durante mis estudios secundarios,me apasioné por la física y las matemáticas, lo que me impulsó a seguir una carrera en el campo de la tecnología y la ciencia.</p>
-                   <p class="eys-parrafo">Mi formación en ciencias y mi pasión por la tecnología me llevaron a decidir estudiar ingeniería en tecnologías de la información en la Universidad Técnica Luis Vargas Torres de Esmeraldas. Allí, he ampliado mis conocimientos en programación y desarrollo de software, aprendiendo lenguajes de programación como C ++ o PHP.</p>
-<p class="eys-parrafo"> Mi objetivo como profesional de la informática es continuar desarrollando habilidades en el campo de la tecnología y la innovación, trabajando en proyectos que impacten positivamente en la sociedad y en el mundo empresarial. Además, como persona, aspiro a ser un ciudadano comprometido y responsable, siempre buscando oportunidades para contribuir al bienestar de mi comunidad.</p>
+                   <p class="eys-parrafo">Me gradué en el Colegio Don Bosco como bachiller en ciencias. Durante mi formación académica, he adquirido habilidades en programación, análisis de datos y diseño de sistemas informáticos. Me interesa mucho el desarrollo de software y la implementación de soluciones tecnológicas innovadoras. Me gusta estar al día en las últimas tendencias y avances en tecnología y me esfuerzo por aplicarlos en mis proyectos y trabajos.</p>
+               
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
     
 
 
-                   <p  class="eys-parrafo">Soy una persona apasionada por la tecnología y me gusta estar al día con las últimas tendencias en el mundo de la informática. .</p> 
-                   <p  class="eys-parrafo">Tengo habilidades en sistemas informaticos y hadware, lo que me permite ser más eficiente en mi trabajo y en el uso de herramientas digitales en mi día a día.</p>
+                   <p  class="eys-parrafo">Soy una persona con conocimientos básicos en informática. Manejo con soltura el sistema operativo Windows y tengo habilidades en el uso de programas como Microsoft Word, Excel y PowerPoint. Además, cuento con conocimientos básicos en navegación web y redes sociales, así como en el uso de correo electrónico y mensajería instantánea.</p> 
+                   <p  class="eys-parrafo">He adquirido habilidades básicas en programación a través de cursos en línea, y tengo conocimientos básicos en lenguajes como HTML, CSS y JavaScript. También tengo habilidades en la creación y edición de imágenes y videos simples mediante herramientas como Canva y Filmora.</p>
                    <p  class="eys-parrafo">Además, me gusta aprender sobre seguridad informática y cómo proteger mi información personal en línea. Estoy en constante aprendizaje y siempre busco mejorar mis habilidades para poder enfrentar nuevos retos en el campo de la tecnología.</p>
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-                   <p   class="eys-parrafo">Soy una persona activa y me encanta disfrutar de mi tiempo libre realizando actividades que me permitan mantenerme en forma y relajado. Uno de mis hobbies favoritos es el deporte, en especial el fútbol y el baloncesto. Me gusta jugar con amigos los fines de semana y ver partidos en la televisión. </p>
+                   <p   class="eys-parrafo">Me gusta mucho leer libros de ciencia ficción y fantasía, especialmente en inglés para mejorar mi fluidez en el idioma. También disfruto de ver series y películas en plataformas de streaming, especialmente aquellas relacionadas con el universo de superhéroes y la tecnología. En mi tiempo libre me gusta practicar deportes al aire libre, como correr y andar en bicicleta. Además, me encanta viajar y conocer nuevas culturas, probar su gastronomía y explorar sus lugares turísticos.
+
+ </p>
        
 
                    
@@ -81,9 +82,9 @@ include("marco-left.php")
  
 <script >  
 
- document.title="Alan Ramirez"; 
-document.getElementById("foto").src="./images/RamirezAlan.jpeg";
-document.getElementById("elnombre").innerHTML="Alan Ramirez";
+ document.title="Isaac Franco"; 
+document.getElementById("foto").src="./images/FrancoIsaac.jpeg";
+document.getElementById("elnombre").innerHTML="Isaac Franco";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
